691b8f39dbd3dd2c8828aee929c8e6e6c87fb733
alex <05sasha50@mail.ru>
2019-05-19 18:28:13 +0300

    Add composer.json

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -4,7 +4,32 @@
 namespace Scheduler\DependencyInjection;
 
 
-class Configuration
-{
+use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
 
+class Configuration implements ConfigurationInterface
+{
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder('scheduler');
+
+        /** @var ArrayNodeDefinition $root */
+        $root = $treeBuilder->getRootNode();
+        $root
+            ->children()
+                ->arrayNode('works')
+                    ->arrayPrototype()
+                        ->children()
+                            ->scalarNode('service')->end()
+                            ->scalarNode('method')->end()
+                            ->scalarNode('interval')->cannotBeEmpty()->end()
+                            ->scalarNode('enable')->defaultFalse()->end()
+                        ->end()
+                    ->end()
+                ->end()
+            ->end();
+
+        return $treeBuilder;
+    }
 }
--- src/DependencyInjection/SchedulerExtension.php
+++ src/DependencyInjection/SchedulerExtension.php
@@ -4,7 +4,15 @@
 namespace Scheduler\DependencyInjection;
 
 
-class SchedulerExtension
-{
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 
+class SchedulerExtension extends Extension
+{
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $configuration = $this->getConfiguration($configs, $container);
+        $config = $this->processConfiguration($configuration, $configs);
+        $a = 10;
+    }
 }
