33b435d7e3b034d796d3ceaba64f08d27afffc43
alex <05sasha50@mail.ru>
2019-05-19 18:27:21 +0300

    Add simple configuration

---
+++ src/DependencyInjection/SchedulerExtension.php
@@ -0,0 +1,10 @@
+<?php
+
+
+namespace Scheduler\DependencyInjection;
+
+
+class SchedulerExtension
+{
+
+}
---
+++ src/SchedulerBundle.php
@@ -0,0 +1,12 @@
+<?php
+
+
+namespace Scheduler;
+
+
+use \Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class SchedulerBundle extends Bundle
+{
+
+}
